95312135ffd5f82b9d560ab63d9b12d402a45879
Matthew Allen <matthewallenunc@gmail.com>
2019-03-30 20:29:09 -0400

    JS Variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,12 @@
 var wordOptions = ["word", "word", "word", "word", "word", "word", "word"];
 var selectedWord = "";
 var LettersInWord = [];
-var numBlanks
+var numBlanks = 0;
+var blanksAndCorrect = [];
+var incorrectLetters = [];
+var wins = 0;
+var losses = 0;
+var remainingGuesses = 9;
 
 
 //Functions
