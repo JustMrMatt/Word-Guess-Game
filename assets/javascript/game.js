a7111e62db2a776ccc695b5711c323a025efd772
Matthew Allen <matthewallenunc@gmail.com>
2019-03-30 20:56:35 -0400

    More functions and starting processes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //Globals
 // --------------------------------------------------------
-var words = ["Hodor Hodor", "Hodor", "Hodor Hodor", "Hodor", "Hodor Hodor Hodor", "Hodor", "Hodor"];
+var words = ["Hodor", "Hodor Hodor", "Hodor Hodor Hodor", "Hold The Door"];
 var selectedWord = "";
 var LettersInWord = [];
 var numBlanks = 0;
@@ -14,22 +14,36 @@ var remainingGuesses = 9;
 //Functions
 // --------------------------------------------------------
 function startGame() {
-    selectedWord = words[Math.floor(Math.random() = words.length)];
+    selectedWord = words[Math.floor(Math.random() * words.length)];
     lettersInWord = selectedWord.split("");
     numBlanks = lettersInWord.length;
 
-    remainingGuesses = 9;
+    remainingGuesses = 8;
     incorrectLetters = [];
     blanksAndCorrect = [];
 
-    
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    document.getElementById("wordToGuess").innerHTML = blanksAndCorrect.join(" ");
+    document.getElementById("numGuesses").innerHTML = remainingGuesses;
+    document.getElementById("winCounter").innerHTML = wins;
+    document.getElementById("lossCounter").innerHTML = losses;
+
+
 
     console.log(selectedWord);
     console.log(lettersInWord);
     console.log(numBlanks);
+    console.log(blanksAndCorrect);
 }
 
 
 //Main Process
 // --------------------------------------------------------
 startGame();
+
+document.onkeyup = function(event) {
+    var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+}
