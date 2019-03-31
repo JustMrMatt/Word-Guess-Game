3ecf4c413e7377326c67b47c282c865d97f7db6e
Matthew Allen <matthewallenunc@gmail.com>
2019-03-30 20:39:20 -0400

    Working through functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //Globals
 // --------------------------------------------------------
-var wordOptions = ["word", "word", "word", "word", "word", "word", "word"];
+var words = ["Hodor Hodor", "Hodor", "Hodor Hodor", "Hodor", "Hodor Hodor Hodor", "Hodor", "Hodor"];
 var selectedWord = "";
 var LettersInWord = [];
 var numBlanks = 0;
@@ -13,6 +13,23 @@ var remainingGuesses = 9;
 
 //Functions
 // --------------------------------------------------------
+function startGame() {
+    selectedWord = words[Math.floor(Math.random() = words.length)];
+    lettersInWord = selectedWord.split("");
+    numBlanks = lettersInWord.length;
+
+    remainingGuesses = 9;
+    incorrectLetters = [];
+    blanksAndCorrect = [];
+
+    
+
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numBlanks);
+}
+
 
 //Main Process
 // --------------------------------------------------------
+startGame();
